8ce83c7dc7a1f521e1576e8997f811cbf9ddb9ad
hugomufraggi <Mufraggi0303>
2022-06-12 17:04:29 +0200

    update schema for user

--- migrations/1_user.sql
+++ migrations/1_user.sql
@@ -1,5 +1,7 @@
-CREATE TABLE persons (
-    id int,
-    name varchar(255),
+CREATE TABLE users (
+    id varchar(36),
+    first_name varchar(255),
+    last_name varchar(255),
+    birthday_date Date,
     city varchar(255)
 );
